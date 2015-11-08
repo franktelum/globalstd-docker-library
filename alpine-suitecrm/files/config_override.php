e59f58b0012b884511172a506d00c2cfbfef32d7
FcoMendoza <zaibaiman@hotmail.com>
2015-11-07 19:07:48 -0600

    Agrega cambios

--- alpine-suitecrm/files/config_override.php
+++ alpine-suitecrm/files/config_override.php
@@ -1,17 +1,18 @@
 <?php
   if(null !== $$installer_defaults) {
-    $$installer_defaults['setup_db_host_name']            = $_ENV['DB_PORT_3306_TCP_ADDR'];
-    $$installer_defaults['setup_db_database_name']        = $_ENV['database_name'];
-    $$installer_defaults['setup_db_admin_user_name']      = $_ENV['db_user_name'];
-    $$installer_defaults['setup_db_admin_password']       = $_ENV['db_password'];
-    $$installer_defaults['db_type']                       = $_ENV['db_type'];
+    $$installer_defaults['setup_db_host_name']            = $_ENV['DB_PORT_3306_TCP_ADDR'] . ':3306';
+    $$installer_defaults['setup_db_database_name']        = $_ENV['DB_NAME'];
+    $$installer_defaults['setup_db_admin_user_name']      = $_ENV['DB_USER'];
+    $$installer_defaults['setup_db_admin_password']       = $_ENV['DB_PASS'];
+    $$installer_defaults['db_type']                       = $_ENV['DB_TYPE'];
   }
 
   if(null !== $$sugar_config) {
-    $$sugar_config['dbconfig']['db_port'] = $_ENV['3306'];
-    $$sugar_config['dbconfig']['db_manager'] = $_ENV['db_manager'];
-    $$sugar_config['dbconfig']['db_host_name'] = $_ENV['DB_PORT_3306_TCP_ADDR'];
-    $$sugar_config['dbconfig']['db_user_name'] = $_ENV['db_user_name'];
-    $$sugar_config['dbconfig']['db_password'] = $_ENV['db_password'];
+    $$sugar_config['dbconfig']['db_port']       = $_ENV['3306'];
+    $$sugar_config['dbconfig']['db_manager']    = $_ENV['DB_MANAGER'];
+    $$sugar_config['dbconfig']['db_name']       = $_ENV['DB_NAME'];
+    $$sugar_config['dbconfig']['db_host_name']  = $_ENV['DB_PORT_3306_TCP_ADDR'];
+    $$sugar_config['dbconfig']['db_user_name']  = $_ENV['DB_NAME'];
+    $$sugar_config['dbconfig']['db_password']   = $_ENV['DB_PASS'];
   }
 ?>
